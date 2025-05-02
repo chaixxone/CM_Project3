9843521b2856ef1d9874d0ac3ae1d4e6165d8aa7
MaximKanevskiy <maxkanevskiy430@yandex.ru>
2025-05-02 14:01:07 +0300

    feat: add vector Normal function

--- include/math.hpp
+++ include/math.hpp
@@ -3,5 +3,13 @@
 
 namespace Engine
 {
-
+	sf::Vector2f Normal(const sf::Vector2f& a)
+	{
+		float x = -a.y;
+		float y = a.x;
+		float length = std::sqrt(x * x + y * y);
+		x /= length;
+		y /= length;
+		return sf::Vector2f{ x, y };
+	}
 } // namespace Engine
