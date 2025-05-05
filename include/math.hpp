c728b968358b75b59da2414a5ffa9a54ca893b1f
vg <maxkanevskiy430@yandex.ru>
2025-05-05 16:44:49 +0300

    feat: add dot product

--- include/math.hpp
+++ include/math.hpp
@@ -12,4 +12,9 @@ namespace Engine
 		y /= length;
 		return sf::Vector2f{ x, y };
 	}
+
+	float Dot(const sf::Vector2f& a, const sf::Vector2f& b)
+	{
+		return a.x * b.x + a.y + b.y;
+	}
 } // namespace Engine
