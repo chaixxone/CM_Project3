fc7a391182b571d694a752e28e96afcc525b0b02
vg <maxkanevskiy430@yandex.ru>
2025-05-05 20:54:38 +0300

    refactor: change variables initialisation and tests appearance

--- tests/unittest.cpp
+++ tests/unittest.cpp
@@ -1,4 +1,5 @@
 #include <catch2/catch_all.hpp>
+#include <catch2/catch_approx.hpp>
 #include <math.hpp>
 
 TEST_CASE("Normal", "[math]")
@@ -13,20 +14,21 @@ TEST_CASE("Projection", "[math]")
 {
 	sf::Vector2f origin{ 5.f, 5.f };
 	sf::Vector2f fillerPoint{ 6.f, 5.3f };
-	sf::Vector2f line{ fillerPoint.x - origin.x, fillerPoint.y - origin.y };
+	sf::Vector2f line = fillerPoint - origin;
 	sf::Vector2f normal = Engine::Normal(line);
 
 	sf::Vector2f point{ 7.f, 7.f };
-	sf::Vector2f vectorPoint{ point.x - origin.x, point.y - origin.y };
+	sf::Vector2f vectorPoint = point - origin;
 	float distanceNormal = Engine::Dot(vectorPoint, normal);
 
 	sf::Vector2f productDistanceNormal = normal * distanceNormal;
-	sf::Vector2f projectionPoint{ point.x - productDistanceNormal.x , point.y - productDistanceNormal.y };
+	sf::Vector2f projectionPoint = point - productDistanceNormal;
 	INFO("projectionPoint = (" << projectionPoint.x << ", " << projectionPoint.y << ")");
 
 	sf::Vector2f expected{ 7.38f, 5.715f };
 
 	float tolerance = 1e-2;
 	// assert projection point coordinates is near expected variable coordinates
-	REQUIRE((abs(expected.x - projectionPoint.x) < tolerance && abs(expected.y - projectionPoint.y) < tolerance));
+	REQUIRE(Catch::Approx(projectionPoint.x).margin(tolerance) == expected.x);
+	REQUIRE(Catch::Approx(projectionPoint.y).margin(tolerance) == expected.y);
 }
