d1e4fe117c6011ceb766f294feef44f8ced0a519
vg <maxkanevskiy430@yandex.ru>
2025-05-18 12:44:36 +0300

    feat: define getVertices function with docs

--- include/math.hpp
+++ include/math.hpp
@@ -1,5 +1,7 @@
 #include <cmath>
+#include <vector>
 #include <SFML/System.hpp>
+#include <SFML/Graphics/Shape.hpp>
 
 namespace Engine
 {
@@ -47,5 +49,24 @@ namespace Engine
 	bool CheckCollide(const std::vector<sf::Vector2f> aShapeVertices, const std::vector<sf::Vector2f> bShapeVertices)
 	{
 		return false;
+	}
+
+	/**
+	* @brief Gets all vertex positions from a shape
+	* @param shape: a pointer to shape
+	* @returns a vector with actual vertex coordinates
+	*/
+	std::vector<sf::Vector2f> getVertices(sf::Shape* shape)
+	{
+		int verticesAmount = shape->getPointCount();
+		std::vector<sf::Vector2f> verteces(verticesAmount);
+		sf::Transform transform = shape->getTransform();
+
+		for (int i = 0; i < verticesAmount; i++)
+		{
+			verteces[i] = transform.transformPoint(shape->getPoint(i));
+		}
+
+		return verteces;
 	}
 } // namespace Engine
